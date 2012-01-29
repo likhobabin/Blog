9ac966662ad04bbee59c1f823aa4ad229e3945d0
Ilya Likhobabin <ilya.likhobabin@gmail.com>
2012-01-29 18:30:02 +0200

    Add study proj Blog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'ac8fb29017d3d1a677be8f3a042ae634d2de99004fa5796fbb25b25efd36d57e1a4f14bd743f022ea2ffda4d2bec3e2d7ad9528c80a79acd77879de8dbcd024b'
